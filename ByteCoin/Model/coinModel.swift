b2dc2e55e0e03ef9337c077f7df23e63fde1154e
muhammed yasser al-sayed <muhammedshehap97@gmail.com>
2023-08-31 12:59:56 +0300

    finish

---
+++ ByteCoin/Model/coinModel.swift
@@ -0,0 +1,12 @@
+//
+//  coinModel.swift
+//  ByteCoin
+//
+//  Created by muhammed shehap on 31/08/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Decodable {
+    let rate: Double
+}
